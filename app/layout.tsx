3424e4818af0282ed2074a528ba9aa76f37ad4e3
Natanael Melo <109748470+Natanael135@users.noreply.github.com>
2025-03-21 11:05:42 -0300

    chore: implementando estrutura inicial do projeto.

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+  title: "CarePulse",
+  description: "HealthCare Sistema de Gestão",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={fontSans.className}>{children}</body>
+    </html>
+  );
+}
